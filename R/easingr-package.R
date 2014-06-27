cadf4996496f6941f1026b96aa098dd625bc7ccd
Matt Barry <mrb@softisms.com>
2014-06-27 00:00:00 +0000

    version 1.0.0

---
+++ R/easingr-package.R
@@ -0,0 +1,9 @@
+#' Credit easing policy tool data.
+#' @description Fetches credit easing policy tool data as time series and provides plots.
+#' @details Downloads the credit easing policy tool weekly data in its original CSV format from the Federal Reserve Bank of Cleveland's web site, and then translates that data into an \code{xts} time series object with easing method dollar value histories as columns.  Provides custom \code{lattice} line and area plot functions for data presentation.
+#' @author Matt Barry \email{mrb@@softisms.com}
+#' @references Federal Reserve Bank of Cleveland research data site \url{http://www.clevelandfed.org/research/data/credit_easing/index.cfm}
+#' @keywords FRB credit easing policy
+#' @name easingr
+#' @docType package
+NULL
